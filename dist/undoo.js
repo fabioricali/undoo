fe73773794b652278f640000f1ccc9568bfc13bd
fabioricali <ricalifabio@gmail.com>
2018-02-13 10:32:28 +0100

    v0.1.0

--- dist/undoo.js
+++ dist/undoo.js
@@ -1,4 +1,4 @@
-// [AIV]  Undoo Build version: 0.0.1  
+// [AIV]  Undoo Build version: 0.1.0  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
